86645df946e6ca3d395171feafadb324747d08b2
Bruno Rian Teixeira <155167152+brunorianteix@users.noreply.github.com>
2024-01-03 15:07:56 -0300

    Add files via upload

---
+++ dist/README.md
@@ -0,0 +1,4 @@
+# Alura Fone
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/Bruno-R/pen/eYXNjRL](https://codepen.io/Bruno-R/pen/eYXNjRL).
+
---
+++ dist/dist/script.js
@@ -0,0 +1,25 @@
+const listaDeTeclas = document.querySelectorAll('input[type=button]');
+const inputTel = document.querySelector('input[type=tel]');
+
+for (let indice = 0; indice < listaDeTeclas.length; indice++) {
+  
+  const tecla = listaDeTeclas[indice];
+  
+  tecla.onclick = function () {
+    inputTel.value = inputTel.value + tecla.value;
+  }
+}
+const listaDeTeclas1 = document.querySelectorAll('input[type=button]');
+
+for (let indice = 0; indice < listaDeTeclas1.length; indice++) {
+  const tecla = listaDeTeclas1[indice];
+
+  tecla.onkeydown = function (evento) {
+    if(evento.code === "Enter" || evento.code === "Space") {
+    tecla.classList.add('ativa');
+    }
+  }
+  tecla.onkeyup = function () {
+    tecla.classList.remove('ativa');
+  }
+}
